1d5476811fa92d2185da29a40a1e6be5475e4bdc
Huy Phan <enpiech@gmail.com>
2019-05-04 18:06:39 +0700

    Update .gitignore

---
+++ Assets/Scripts/GunAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+class GunAnimation
+{
+    private const string ROTATING_PARAM = "IsRotating";
+
+    private readonly Animator _animator;
+
+    private readonly int _idIsRotating;
+
+    public GunAnimation(Animator animator)
+    {
+        _animator = animator;
+
+        _idIsRotating = Animator.StringToHash(ROTATING_PARAM);
+    }
+
+    public void StopRotating()
+    {
+        _animator.speed = 0;
+    }
+
+    public void Rotating()
+    {
+        _animator.speed = 1;
+    }
+}
---
+++ Assets/Scripts/SpawnCollectable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCollectable : MonoBehaviour {
+
+    public int xRange = 10;
+    public int yRange = 10;
+    public int numberOfObjects = 16;
+
+    public GameObject[] objects;
+
+    float worldScreenHeight;
+    float worldScreenWidth;
+
+    void Awake()
+    {
+        worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        xRange = Mathf.FloorToInt(worldScreenWidth) / 2;
+        yRange = Mathf.FloorToInt(worldScreenHeight) / 2;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        Spawn();
+	}
+	
+	void Spawn()
+    {
+        for (int i = 0; i < numberOfObjects; ++i)
+        {
+            Vector2 spawnLocation = new Vector2(Random.Range(xRange, -xRange), Random.Range(yRange - 10, -yRange));
+
+            int objectPick = Random.Range(1, objects.Length);
+            Instantiate (objects[objectPick], spawnLocation, Quaternion.identity);
+        }
+    }
+}
